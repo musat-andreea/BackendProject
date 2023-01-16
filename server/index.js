5794c485a0d914862a77cf80a540088325c604be
BetyDobre <bettydobre@gmail.com>
2023-01-16 15:01:47 +0200

    configured redis and enabled cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const session = require("express-session");
 const mongoose = require("mongoose");
+const redis = require("redis");
+const RedisStore = require("connect-redis")(session);
+const cors = require("cors");
 const doctors = require("./doctors.js");
 const pacients = require("./pacients.js");
 const prescriptions = require("./prescriptions.js");
@@ -23,13 +27,13 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-const { PORT, MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, REDIS_URL, REDIS_PORT, SESSION_SECRET } = require("./config/config");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, REDIS_URL, REDIS_PORT, SESSION_SECRET } = require("./config/config");
 const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 mongoose.set('strictQuery', true);
 
 const connectWithRetry = () => {
   mongoose.connect(mongoUrl)
-      .then(() => console.log("successfully connected to Mongo"))
+      .then(() => console.log("Successfully connected to Mongo"))
       .catch((e) => {
           console.log("NOT successfully connected to Mongo, retrying...")
           setTimeout(connectWithRetry, 5000);
@@ -37,7 +41,27 @@ const connectWithRetry = () => {
 }
 connectWithRetry();
 
+const redisClient = redis.createClient({
+  url: `redis://${REDIS_URL}:${REDIS_PORT}`,
+  legacyMode: true,
+});
+redisClient.connect().then(() => { console.log('Successfully connected to Redis') })
+
+app.use(session({
+  store: new RedisStore({ client: redisClient }),
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+      secure: false, // if true only transmit cookie over https
+      httpOnly: true, // if true prevent client side JS from reading the cookie 
+      maxAge: 1000 * 30 // 1000 * 60 * 10 // session max age in miliseconds
+  }
+}));
+
 app.use(express.json());
+app.enable("trust proxy");
+app.use(cors({}));
 app.use('/', doctors);
 app.use('/', pacients);
 app.use('/', prescriptions);
