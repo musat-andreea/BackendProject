18a36efd24217272277d3c1329350db784542bf7
DESKTOP-FB3L1R3\oracle <alinamocanu2@gmail.com>
2023-01-16 09:59:46 +0200

    push CRUD endpoints

--- server/index.js
+++ server/index.js
@@ -1,29 +1,31 @@
 const express = require("express");
+const doctors = require("./doctors.js");
+const pacients = require("./pacients.js");
+const prescriptions = require("./prescriptions.js");
+const appointments = require("./appointments.js");
 
 const PORT = process.env.PORT || 8008;
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
-
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
+app.use(express.json());
+app.use('/', doctors);
+app.use('/', pacients);
+app.use('/', prescriptions);
+app.use('/', appointments);
 
 const swaggerUi = require("swagger-ui-express");
 swaggerDocument = require("../swagger.json");
 
 app.use(
-    '/api-docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument)
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument),
 );
-//
-// app.listen(PORT, () => {
-//     console.log(`Server listening on ${PORT}`);
-// });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
 
 // const dotenv = require('dotenv').config()
 
